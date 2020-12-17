4bc591960ba21374baa1eb858d354aaa621000e9
geris <73652445+Gerisbr@users.noreply.github.com>
2020-12-16 23:18:51 -0300

    Add files via upload

---
+++ js/ex4.js
@@ -0,0 +1,7 @@
+for(let i = 1 ; i <= 20; i++){
+    document.querySelector("#contagemVertical").innerHTML += i + "<br>";
+}
+
+for(let i = 1 ; i <= 20; i++){
+    document.querySelector("#contagemHorizontal").innerText += i + "-";
+}
